ebf1cb5d583c56cedcfd81a8804e17e7ad794b5f
Arian Allenson Valdez <Secretmapper16@gmail.com>
2019-10-28 09:15:01 +0800

    Format taskStats

--- src/hooks/db/index.ts
+++ src/hooks/db/index.ts
@@ -2,14 +2,19 @@ import { useDB } from 'react-pouchdb'
 import usePromise from 'react-use-promise'
 import { ISODate } from '../../utils/time'
 
-export type ITaskStats = {
-  rows: Array<{ value: number; key: string }>
-}
+type TaskStat = { x: string; y: number; label: string }
+export type ITaskStats = [
+  { [key: string]: Array<TaskStat> },
+  Array<{ value: number; tag: string }>
+]
+type queryResults = { value: number; key: [string, string] }
 
 export const useTaskStats = (): ITaskStats => {
   const db = useDB('tasks')
   const startkey = new Date()
   startkey.setDate(new Date().getDate() - 3)
+  const endkey = new Date()
+  endkey.setDate(new Date().getDate() + 1)
 
   const [result] = usePromise(
     () =>
@@ -17,21 +22,41 @@ export const useTaskStats = (): ITaskStats => {
         {
           map: function (doc: any, emit: any) {
             for (let i = 0; i < doc.tags.length; i++) {
-              emit(doc.tags[i], doc.duration)
+              emit([doc.date, doc.tags[i]], doc.duration)
             }
           },
           reduce: '_sum'
         },
         {
-          startkey: ISODate(startkey),
-          endkey: ISODate(new Date()),
+          startkey: [ISODate(startkey), ''],
+          endkey: [ISODate(endkey), ''],
           group: true,
-          group_level: 1,
+          group_level: 2,
           reduce: true
         }
       ),
     []
   )
 
-  return result
+  const hash: { [key: string]: Array<TaskStat> } = {}
+  if (result) {
+    result.rows.map(({ value, key }: queryResults) => {
+      const [date, tag] = key
+      if (!(tag in hash)) {
+        hash[tag] = []
+      }
+
+      hash[tag].push({ x: date, y: value, label: tag })
+    })
+  }
+  const sumHashValues = (o: Array<TaskStat>) =>
+    o.reduce((acc: number, i) => acc + i.y, 0)
+  const tags: ITaskStats[1] = result
+    ? Object.values(hash).map(o => ({
+        tag: o[0].label,
+        value: sumHashValues(o)
+      }))
+    : []
+
+  return [hash, tags]
 }
