8f8bc80515ab85095a866acce9445984517428b7
Arian Allenson Valdez <Secretmapper16@gmail.com>
2019-10-28 09:21:40 +0800

    Visualize stats

--- src/components/TaskStats/index.tsx
+++ src/components/TaskStats/index.tsx
@@ -1,18 +1,62 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import styled from 'styled-components'
 import TaskTag from '../TaskTag'
+import { useTaskStats } from '../../hooks/db'
+import { ISODate, msToMinutes, msToHours } from '../../utils/time'
+
+import { VictoryChart, VictoryAxis, VictoryStack, VictoryBar } from 'victory'
 
 const TaskStats: React.FC = () => {
   return (
     <Container>
       <TaskStatsDate>Oct 7 - Oct 11</TaskStatsDate>
       <HeatmapContainer />
-      <div>
-        <TaskTag>piano</TaskTag>
-        <TaskTagTime>16 hrs</TaskTagTime>
-      </div>
+      <Suspense fallback=''>
+        <TaskList />
+      </Suspense>
     </Container>
   )
+}
+
+const TaskList: React.FC = () => {
+  const [stats, tags] = useTaskStats()
+
+  let d: any = new Date()
+  d.setDate(new Date().getDate() - 1)
+  d = ISODate(d)
+
+  let da: any = new Date()
+  da.setDate(new Date().getDate())
+  da = ISODate(da)
+
+  return (
+    <div>
+      <VictoryChart domainPadding={{ x: 50 }}>
+        <VictoryStack
+          colorScale={['gold', 'orange', 'tomato']}
+          style={victoryStyles}
+        >
+          {Object.keys(stats).map(tag => (
+            <VictoryBar key={tag} data={stats[tag]} />
+          ))}
+        </VictoryStack>
+        <VictoryAxis />
+      </VictoryChart>
+      {tags.map(({ value, tag }) => (
+        <div key={tag}>
+          <TaskTag>{tag}</TaskTag>
+          <TaskTagTime>
+            {msToHours(value)}h {msToMinutes(value)}m
+          </TaskTagTime>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const victoryStyles = {
+  data: { width: 30 },
+  labels: { padding: -20 }
 }
 
 const Container = styled.div``
